f306be2483102950d85ec3394233b4299631de8f
Juan de Urtubey <jdeurt@gmail.com>
2023-01-24 19:45:24 -0600

    chore: remove redundant object key type

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 export type { Compose } from "./types/compose";
 export type { ElementOf } from "./types/element-of";
 export type { Intersection } from "./types/intersection";
-export type { ObjectKey } from "./types/object-key";
 export type { PathOf, ResolvePath } from "./types/object-path";
 export type { PartialIntersection } from "./types/partial-intersection";
 export type { PropertyOf } from "./types/property-of";
--- src/types/object-key.ts
+++
@@ -1,1 +0,0 @@
-export type ObjectKey = string | number | symbol;
